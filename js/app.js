9ee5e97eb772164938dad8f7c7ecd2b27cc5d8e8
Renato Afonso <renato.roberto@gmail.com>
2018-03-04 19:04:38 +0000

    add buildDeck
    code not tested: 1st iteration

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
+let cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
 
 
 /*
@@ -10,11 +11,29 @@
  *   - add each card's HTML to the page
  */
 
- const cards = document.querySelectorAll('.card');
- cards.shuffle();
+document.addEventListener("DOMContentLoaded", buildDeck()); //when DOM is loaded calls build deck function
 
+function buildDeck() {
+    const newList = document.createElement('ul'); //creates new ul without li
 
+    newList.className = 'deck'; //adds class to ul above
+    const mainContain = document.querySelector('.container');
 
+    mainContain.appendChild('newList'); //appends ul as last child to main div
+
+    shuffle(cards); //shuffles the cards array
+
+    for (let i = 0; i <= cards.Length - 1; i++) { //creates 16 cards
+        const listItem = document.createElement('li');
+
+        listItem.className = 'card'; //adds class to li
+
+        listItem.innerHTML = '<i class="fa" ' + cards[i] + '></i>'; //adds fontawesome through array new order to li, thus creating the ul inside the comments of HTML file
+        newList.appendChild(listItem);
+    }
+}
+
+// document.body.appendChild(fragment);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -42,3 +61,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+// https://github.com/shannonj498/memory-matching-game/blob/master/js/app.js
